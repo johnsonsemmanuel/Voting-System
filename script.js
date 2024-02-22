b228899a98caa80cd0b0df498cb3755b9f96989e
johnsonsemmanuel <johnsonsemmanuel@gmail.com>
2024-02-22 09:58:10 +0000

    new codes

--- script.js
+++ script.js
@@ -24,38 +24,38 @@ function submitVote() {
 
   const optionId = selectedOption.value;
 
-  const selectOptionObj = options.find((option) => option.id == optionId);
+  const selectOptionObj = options.find((option) => option.id === optionId);
 
   if (selectOptionObj) {
     selectOptionObj.votes++;
     console.log(selectOptionObj);
-    // display results
+    //display results
     displayResult()
   }
 }
 
 function displayResult() {
-    const result = document.getElementById("result")
-    result.innerHTML = "";
+  const result = document.getElementById("result");
+  result.innerHTML = "";
 
-    options.forEach((option) =>{
-        const percentage = ((option.votes / getTotalVotes())*100).toFixed(2) || 0;
-        const barWidth = percentage > 0 ? percentage + "%" : "0%";
+  options.forEach((option) => {
+    const percentage = ((option.votes / getTotalVotes()) * 100).toFixed(2) || 0;
+    const barWidth = percentage > 0 ? percentage + "%" : "0%";
 
-        const optionResult = document.createElement("div")
-        optionResult.className = "option-result"
-        optionResult.innerHTML = `<span class="option-text"></span>
-        <div class="bar-container">
-            <div class="bar" style="width: ${barWidth}"></div>
-        </div>
-        <span class="percentage">${percentage}%</span>`
+    const optionResult = document.createElement("div");
+    optionResult.className = "option-result";
+    optionResult.innerHTML = `   <span class="option-text"></span>
+    <div class="bar-container">
+      <div class="bar" style="width: ${barWidth}"></div>
+    </div>
+    <span class="percentage">${percentage}</span>`;
 
-        result.appendChild(optionResult)
-    })
+    result.appendChild(optionResult)
+  });
 }
 
 function getTotalVotes() {
-    return options.reduce((total, option) => total + option.votes, 0)
+  return options.reduce((total, option) => total + option.votes, 0);
 }
 
 displayResult()
